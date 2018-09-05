512d6b2d601fbc4cb99dad23b128618a1c0c6406
Daniel Toebe <dtoebe@gmail.com>
2018-09-04 23:17:00 -0700

    add cli flags and string builder

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-use std::env;
+extern crate clap;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
 use std::str;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        println!("Usage here...");
-        process::exit(1);
-    }
+use clap::{App, Arg};
 
-    let file_path = args[1].clone();
+fn main() {
+    let m = App::new("wc-rs")
+        .version("0.1.0")
+        .author("Daniel Toebe <dtoebe@gmail.com>")
+        .about("a wc clone in rust")
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("print the word counts"),
+        ).arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("print the newline counts"),
+        ).arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("print the newline counts"),
+        ).arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("print the newline counts"),
+        ).arg(
+            Arg::with_name("INPUT")
+                .help("file to count from")
+                .required(true)
+                .takes_value(true),
+        ).get_matches();
+
+    let file_path = m.value_of("INPUT").unwrap();
 
     let mut file = File::open(&file_path).unwrap_or_else(|e| {
         println!("failed to open file: {}", e);
@@ -19,7 +47,7 @@ fn main() {
     });
 
     let mut bytes: Vec<u8> = Vec::new();
-    let n_bytes = match file.read_to_end(&mut bytes) {
+    let n = match file.read_to_end(&mut bytes) {
         Ok(n) => n,
         Err(e) => {
             println!("failed to read file: {}", e);
@@ -27,17 +55,40 @@ fn main() {
         }
     };
 
-    let n_lines = bytes.iter().filter(|&b| *b == b'\n').count();
-    let n_chars = bytes.iter().map(|b| *b as char).count();
-    let f = str::from_utf8(&bytes).unwrap_or_else(|e| {
-        println!("failed to conver to string: {}", e);
-        process::exit(1);
-    });
+    let mut output = String::new();
 
-    let n_words = f.split_whitespace().count();
+    let mut none: bool = false;
+    if !m.is_present("words")
+        && !m.is_present("lines")
+        && !m.is_present("bytes")
+        && !m.is_present("chars")
+    {
+        none = true;
+    }
 
-    println!("{} {}", n_bytes, file_path);
-    println!("{} {}", n_words, file_path);
-    println!("{} {}", n_chars, file_path);
-    println!("{} {}", n_lines, file_path);
+    if m.is_present("words") || none {
+        let f = str::from_utf8(&bytes).unwrap_or_else(|e| {
+            println!("failed to conver to string: {}", e);
+            process::exit(1);
+        });
+
+        let n_words = f.split_whitespace().count();
+        output.push_str(&format!("{} ", n_words));
+    }
+
+    if m.is_present("lines") || none {
+        let n_lines = bytes.iter().filter(|b| **b == b'\n').count();
+        output.push_str(&format!("{} ", n_lines));
+    }
+
+    if m.is_present("bytes") || none {
+        output.push_str(&format!("{} ", n));
+    }
+
+    if m.is_present("chars") || none {
+        let n_chars = bytes.iter().map(|b| *b as char).count();
+        output.push_str(&format!("{} ", n_chars));
+    }
+
+    println!("{} {}", output, file_path);
 }
